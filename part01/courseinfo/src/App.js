59bced57202c8155f6f842d25694c2ca0d429469
Beshoy Shehata <0xbeshoy@gmail.com>
2022-04-11 01:22:29 +0200

    1.2

--- part01/courseinfo/src/App.js
+++ part01/courseinfo/src/App.js
@@ -6,20 +6,22 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercises={props.part1} />
+      <Part part={props.part2} exercises={props.part2} />
+      <Part part={props.part3} exercises={props.part3} />
     </div>
   )
 }
 
-
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} - {props.exercises}
+      </p>
+    </div>
+  )
+}
 
 const Total = (props) => {
   return (
